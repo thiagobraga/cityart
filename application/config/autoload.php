eff30c350db97ac7b0541bcc030b25648daf0728
Thiago Braga <contato@thiagobraga.org>
2014-09-26 08:11:40 -0300

    Autoloading CodeIgniter resources and other features.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -67,7 +67,10 @@ $autoload['libraries'] = array(
  *
  *  $autoload['helper'] = array('url', 'file');
  */
-$autoload['helper'] = array();
+$autoload['helper'] = array(
+    'url',
+    'utility'
+);
 
 /**
  * -------------------------------------------------------------------
