5172bab433e197a41c261016b66818aaf8b89de7
Thiago Braga <contato@thiagobraga.org>
2014-12-23 01:58:06 -0200

    Removing unnecessary files

--- application/views/_template/header.php
+++
@@ -1,8 +0,0 @@
-<header>
-    <section class="container">
-        <img src="assets/images/logo/logo-white.png"
-            class="logo"
-            width="300"
-            height="113" />
-    </section>
-</header>
--- application/views/landing/form.php
+++
@@ -1,8 +0,0 @@
-<form id="landing-page" role="form">
-    <div class="form-group">
-        <label for="">label</label>
-        <input type="text" class="form-control" id="" placeholder="Input field">
-    </div>
-
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
