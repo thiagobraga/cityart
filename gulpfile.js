6fb4b9e0fb9ab85b94ef88b0a693909b810f5d26
Thiago Braga <contato@thiagobraga.org>
2015-06-10 03:03:24 -0300

    Avoiding errors to stop the script execution on gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,13 @@ var gulp         = require('gulp'),
     sass         = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     browserSync  = require('browser-sync'),
+    plumber      = require('gulp-plumber'),
     reload       = browserSync.reload,
 
     paths = {
         styles: {
             src:   'assets/sass',
-            files: 'assets/sass/**/*.scss',
+            files: 'assets/sass/*.scss',
             main:  'assets/sass/styles.scss',
             dest:  'assets/css/dist'
         },
@@ -41,7 +42,14 @@ gulp.task('browser-sync', function () {
 // Styles
 gulp.task('styles', function () {
     return gulp.src(paths.styles.main)
+        .pipe(plumber())
         .pipe(sass({ style: 'compressed' }))
+        .on('error', function (err) {
+            console.log('\nError: ' + err.message);
+            console.log('File: ' + err.fileName);
+            console.log('Line: ' + err.lineNumber + '\n');
+            this.emit('end');
+        })
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(gulp.dest(paths.styles.dest))
         .pipe(reload({ stream: true }));
