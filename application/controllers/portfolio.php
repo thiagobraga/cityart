ba7d8439e431e01c29de34c3a3bec3fd10e5a6d7
Thiago Braga <contato@thiagobraga.org>
2015-07-06 00:08:32 -0300

    Creating portfolio page and adjusting content background color

--- application/controllers/portfolio.php
+++ application/controllers/portfolio.php
@@ -1,30 +1,5 @@
 <?php
 
-/**
- * Portfolio.php
- *
- * Classes, métodos e propriedades do controller Portfolio.
- * A classe Portfolio estende a classe MY_Controller.
- *
- * PHP version 5
- *
- * LICENSE: This source file is subject to version 3.01 of the PHP license
- * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
- * the PHP License and are unable to obtain it through the web, please
- * send a note to license@php.net so we can mail you a copy immediately.
- *
- * @category  Geoprocessamento de dados
- * @package   Barpedia
- * @author    Matheus Cesario <matheus@institutosoma.org.br>
- * @author    Thiago Braga <thiago@institutosoma.org.br>
- * @copyright 1997-2005 The PHP Group
- * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
- * @version   GIT: $Id$
- * @link      http://barpedia.org
- * @since     File available since Release 0.0.0
- */
-
 defined('BASEPATH') || exit('No direct script access allowed');
 
 /**
@@ -40,6 +15,11 @@ defined('BASEPATH') || exit('No direct script access allowed');
 class Portfolio extends CityArt
 {
 
+    /**
+     * [index description]
+     *
+     * @return  [type]
+     */
     public function index()
     {
         $this->data = array_merge($this->data, array(
