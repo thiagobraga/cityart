5c843bb790f90f4244bd359d4f053a7d8ee1e240
Thiago Braga <thibraga06@gmail.com>
2015-07-24 18:53:39 -0300

    Creating the team section

---
+++ application/views/home/who-we-are.php
@@ -0,0 +1,39 @@
+<h1 class="col-xs-12">Nossa Equipe</h1>
+
+<div class="row">
+    <div class="col-xs-12 col-md-4">
+        <div class="panel panel-default panel-image">
+            <img src="http://666a658c624a3c03a6b2-25cda059d975d2f318c03e90bcf17c40.r92.cf1.rackcdn.com/unsplash_52d09387ae003_1.JPG" class="panel-image-preview" />
+        </div>
+    </div>
+
+     <div class="col-xs-12 col-md-4">
+        <div class="panel panel-default panel-image">
+            <img src="http://666a658c624a3c03a6b2-25cda059d975d2f318c03e90bcf17c40.r92.cf1.rackcdn.com/unsplash_52cf9489095e8_1.JPG" class="panel-image-preview" />
+        </div>
+    </div>
+
+    <div class="col-xs-12 col-md-4">
+        <div class="panel panel-default panel-image">
+            <img src="http://666a658c624a3c03a6b2-25cda059d975d2f318c03e90bcf17c40.r92.cf1.rackcdn.com/unsplash_52c470899a2e1_1.JPG" class="panel-image-preview" />
+        </div>
+    </div>
+
+    <div class="col-xs-12 col-md-4">
+        <div class="panel panel-default panel-image">
+            <img src="http://666a658c624a3c03a6b2-25cda059d975d2f318c03e90bcf17c40.r92.cf1.rackcdn.com/unsplash_52cd36aac6bed_1.JPG" class="panel-image-preview" />
+        </div>
+    </div>
+
+    <div class="col-xs-12 col-md-4">
+        <div class="panel panel-default panel-image">
+            <img src="http://cdn.morguefile.com/imageData/public/files/f/FidlerJan/01/l/1390332147k5jmx.jpg" class="panel-image-preview" />
+        </div>
+    </div>
+
+    <div class="col-xs-12 col-md-4">
+        <div class="panel panel-default panel-image">
+            <img src="http://cdn.morguefile.com/imageData/public/files/b/butkovicdub/12/l/1387210234czosi.jpg" class="panel-image-preview" />
+        </div>
+    </div>
+</div>
