20dc0665e3d20c65b67ea405f8ba8360dc55543f
Thiago Braga <thibraga06@gmail.com>
2017-01-17 04:57:49 -0200

    Pasta db sem utilidade

--- db/deploy.sh
+++
@@ -1,3 +0,0 @@
-mysql -uroot -p"s0cio.br4g4,pa55" -e "source D:/Projetos/Vhosts/bptadvogados/database/schema.sql" -v -v -v -t --default-character-set=utf8
-mysql -uroot -p"s0cio.br4g4,pa55" -e "source D:/Projetos/Vhosts/bptadvogados/database/core.sql" -v -v -v -t --default-character-set=utf8
-mysql -uroot -p"s0cio.br4g4,pa55" -e "source D:/Projetos/Vhosts/bptadvogados/database/data.sql" -v -v -v -t --default-character-set=utf8
