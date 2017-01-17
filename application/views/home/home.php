73f236d64865f6369519976623e1cba726fe989d
Thiago Braga <thibraga06@gmail.com>
2017-01-17 05:00:01 -0200

    Removendo seções inutilizadas

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,5 +1,3 @@
 <div class="content container">
     <?php $this->load->view('home/about') ?>
-    <?php $this->load->view('home/portfolio') ?>
-    <?php $this->load->view('home/who-we-are') ?>
 </div>
