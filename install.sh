2392d7788e2750baf5446c5ef65b5fc1235474aa
Thiago Braga <thibraga06@gmail.com>
2017-01-18 04:49:41 -0200

    Refatoração no install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,2 @@
-# Remove previous node modules and bower components folders
-rm -rf node_modules/
-rm -rf assets/bower/
-
-# Install node.js modules from package.json
-npm install --save-dev
-
-# Install bower components
-bower install --save --allow-root
-
-# Run grunt
-grunt
+#!/bin/sh
+rm -rf node_modules && npm i && gulp
