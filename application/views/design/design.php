ce406a4158db3d98566c38a898fed756112e9ee7
Thiago Braga <thibraga06@gmail.com>
2017-01-18 23:31:59 -0200

    Alterações no tamanho e cores dos textos e correção no gradiente.

--- application/views/design/design.php
+++ application/views/design/design.php
@@ -1,1 +1,15 @@
-<div class="content container"></div>
+<div class="content container">
+    <div class="row">
+        <div class="col-xs-6 text-justify">
+            <h2>Processo Criativo</h2>
+            <p>Criatividade e inspiração são as chaves para o sucesso de um projeto visual. Com uma linguagem adequada, a sua marca se destaca no mercado e ganha posicionamento em relação ao público-alvo que pretende atingir.</p>
+            <p>Desde a concepção do logotipo, buscando sintetizar os conceitos que sua empresa pretende transmitir, até o desenvolvimento de todo material necessário, nós estamos preparados para formular idéias criativas e inovadoras para promover o seu negócio.</p>
+            <p>Nosso conhecimento técnico em processos gráficos nos permite garantir o sucesso do produto final, dando suporte adequado para a produção do seu material.</p>
+            <p>Estamos prontos para colorir as suas idéias!</p>
+        </div>
+
+        <div class="col-xs-6">
+            <img src="assets/images/photos/about-photo.jpg" class="img-responsive" />
+        </div>
+    </div>
+</div>
