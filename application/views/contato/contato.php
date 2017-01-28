6e1a2138a458b390f301d53ceada621bb0e2a5de
Thiago Braga <thibraga06@gmail.com>
2017-01-28 04:50:49 -0200

    Exibindo telefone acima do mapa

--- application/views/contato/contato.php
+++ application/views/contato/contato.php
@@ -1,4 +1,4 @@
-<div class="col-xs-12 col-md-6 contact-page">
+<div class="col-xs-12 col-md-5">
     <h2>Contato</h2>
 
     <form id="contact-form" class="form-horizontal" role="form">
@@ -35,7 +35,12 @@
     </form>
 </div>
 
-<div class="col-xs-12 col-md-6">
+<div class="col-xs-12 col-md-7">
+    <h2>
+        <i class="fa fa-phone icon"></i> 14 3208-5654
+        <i class="fa fa-whatsapp icon"></i> 14 99777-4155
+    </h2>
+
     <map class="map-md"
         center="-22.3319145, -49.0296082"
         zoom="17"
@@ -44,7 +49,7 @@
         draggable="true"
         scrollwheel="true"
         keyboard-shortcuts="true"
-        styles="[{'featureType':'all','elementType':'labels','stylers':[{'visibility':'on'}]},{'featureType':'all','elementType':'labels.text.fill','stylers':[{'saturation':36},{'color':'#000000'},{'lightness':40}]},{'featureType':'all','elementType':'labels.text.stroke','stylers':[{'visibility':'on'},{'color':'#000000'},{'lightness':16}]},{'featureType':'all','elementType':'labels.icon','stylers':[{'visibility':'off'}]},{'featureType':'administrative','elementType':'geometry.fill','stylers':[{'color':'#000000'},{'lightness':20}]},{'featureType':'administrative','elementType':'geometry.stroke','stylers':[{'color':'#000000'},{'lightness':17},{'weight':1.2}]},{'featureType':'administrative.country','elementType':'labels.text.fill','stylers':[{'color':'#e5c163'}]},{'featureType':'administrative.locality','elementType':'labels.text.fill','stylers':[{'color':'#c4c4c4'}]},{'featureType':'administrative.neighborhood','elementType':'labels.text.fill','stylers':[{'color':'#e5c163'}]},{'featureType':'landscape','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':20}]},{'featureType':'poi','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':21},{'visibility':'on'}]},{'featureType':'poi.business','elementType':'geometry','stylers':[{'visibility':'on'}]},{'featureType':'road.highway','elementType':'geometry.fill','stylers':[{'color':'#e5c163'},{'lightness':'0'}]},{'featureType':'road.highway','elementType':'geometry.stroke','stylers':[{'visibility':'off'}]},{'featureType':'road.highway','elementType':'labels.text.fill','stylers':[{'color':'#ffffff'}]},{'featureType':'road.highway','elementType':'labels.text.stroke','stylers':[{'color':'#e5c163'}]},{'featureType':'road.arterial','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':18}]},{'featureType':'road.arterial','elementType':'geometry.fill','stylers':[{'color':'#575757'}]},{'featureType':'road.arterial','elementType':'labels.text.fill','stylers':[{'color':'#ffffff'}]},{'featureType':'road.arterial','elementType':'labels.text.stroke','stylers':[{'color':'#2c2c2c'}]},{'featureType':'road.local','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':16}]},{'featureType':'road.local','elementType':'labels.text.fill','stylers':[{'color':'#999999'}]},{'featureType':'transit','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':19}]},{'featureType':'water','elementType':'geometry','stylers':[{'color':'#000000'},{'lightness':17}]}]">
+        styles="[{featureType:'all',elementType:'labels',stylers:[{visibility:'on'}]},{featureType:'all',elementType:'labels.text.fill',stylers:[{saturation:36},{color:'#000000'},{lightness:40}]},{featureType:'all',elementType:'labels.text.stroke',stylers:[{visibility:'on'},{color:'#000000'},{lightness:16}]},{featureType:'all',elementType:'labels.icon',stylers:[{visibility:'off'}]},{featureType:'administrative',elementType:'geometry.fill',stylers:[{color:'#000000'},{lightness:20}]},{featureType:'administrative',elementType:'geometry.stroke',stylers:[{color:'#000000'},{lightness:17},{weight:1.2}]},{featureType:'administrative.country',elementType:'labels.text.fill',stylers:[{color:'#e5c163'}]},{featureType:'administrative.locality',elementType:'labels.text.fill',stylers:[{color:'#c4c4c4'}]},{featureType:'administrative.neighborhood',elementType:'labels.text.fill',stylers:[{color:'#e5c163'}]},{featureType:'landscape',elementType:'geometry',stylers:[{color:'#000000'},{lightness:20}]},{featureType:'poi',elementType:'geometry',stylers:[{color:'#000000'},{lightness:21},{visibility:'on'}]},{featureType:'poi.business',elementType:'geometry',stylers:[{visibility:'on'}]},{featureType:'road.highway',elementType:'geometry.fill',stylers:[{color:'#e5c163'},{lightness:'0'}]},{featureType:'road.highway',elementType:'geometry.stroke',stylers:[{visibility:'off'}]},{featureType:'road.highway',elementType:'labels.text.fill',stylers:[{color:'#ffffff'}]},{featureType:'road.highway',elementType:'labels.text.stroke',stylers:[{color:'#e5c163'}]},{featureType:'road.arterial',elementType:'geometry',stylers:[{color:'#000000'},{lightness:18}]},{featureType:'road.arterial',elementType:'geometry.fill',stylers:[{color:'#575757'}]},{featureType:'road.arterial',elementType:'labels.text.fill',stylers:[{color:'#ffffff'}]},{featureType:'road.arterial',elementType:'labels.text.stroke',stylers:[{color:'#2c2c2c'}]},{featureType:'road.local',elementType:'geometry',stylers:[{color:'#000000'},{lightness:16}]},{featureType:'road.local',elementType:'labels.text.fill',stylers:[{color:'#999999'}]},{featureType:'transit',elementType:'geometry',stylers:[{color:'#000000'},{lightness:19}]},{featureType:'water',elementType:'geometry',stylers:[{color:'#000000'},{lightness:17}]}]">
 
         <marker position="-22.331878, -49.029618"
             animation="Animation.DROP"
