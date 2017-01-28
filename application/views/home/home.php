bf05facdf722454f0a5adf3d5f875e42d94ea916
Thiago Braga <thibraga06@gmail.com>
2017-01-28 04:49:35 -0200

    Imagens com sombra e border-radius

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -8,6 +8,6 @@
     </div>
 
     <div>
-        <img src="assets/images/dist/photos/apresentacao.jpg" class="grid-image" />
+        <img src="assets/images/dist/photos/apresentacao.jpg" class="grid-image image-shadow" />
     </div>
 </div>
