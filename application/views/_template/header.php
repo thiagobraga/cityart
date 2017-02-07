e82c088971598f1cfa53c4a0430e18b3cd0af7d4
Thiago Braga <thibraga06@gmail.com>
2017-02-07 06:36:39 -0200

    Ocultando serviços no header quando a resolução for pequena

--- application/views/_template/header.php
+++ application/views/_template/header.php
@@ -1,10 +1,10 @@
 <header>
     <div class="container">
-        <div class="col-xs-5">
+        <div class="col-xs-12 col-sm-6">
             <a href="<?php echo base_url() ?>" class="logo"></a>
         </div>
 
-        <div class="col-xs-7">
+        <div class="hidden-xs col-sm-6">
             <div class="services">
                 <a href="<?php echo base_url('design') ?>"><i class="icon-design"></i> Design</a>
                 <a href="<?php echo base_url('pre-impressao') ?>"><i class="icon-press"></i> Pré-Impressão</a>
