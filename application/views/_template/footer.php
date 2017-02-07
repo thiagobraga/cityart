21ecf83a1c520a57345ec87c822a1a62e39acbf5
Thiago Braga <thibraga06@gmail.com>
2017-02-07 06:33:27 -0200

    Exibindo telefone e outras informações no footer com ícones

--- application/views/_template/footer.php
+++ application/views/_template/footer.php
@@ -2,8 +2,14 @@
     <div class="container">
         <div class="footer-content">
             <b class="first-line"><span class="text-warning">CityArt</span> Artes Gráficas</b><br/>
-            14 3208-5654 / 99777-4155<br/>
-            <a href="mailto:contato@cityart.com.br">contato@cityart.com.br</a><br/>
+
+            <i class="fa fa-phone"></i> 14 3208-5654
+            <i class="ionicons ion-social-whatsapp"></i> 14 99777-4155<br/>
+
+            <a href="mailto:contato@cityart.com.br">
+                <i class="fa fa-envelope"></i> contato@cityart.com.br
+            </a><br/>
+
             Bauru/SP
         </div>
     </div>
