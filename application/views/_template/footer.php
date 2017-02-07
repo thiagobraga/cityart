858728a65b130a401c365e2277368fd2c8d14cb5
Thiago Braga <thibraga06@gmail.com>
2017-02-07 15:29:11 -0200

    Correções no footer e na tela Pré-Impressão

--- application/views/_template/footer.php
+++ application/views/_template/footer.php
@@ -3,8 +3,8 @@
         <div class="footer-content">
             <b class="first-line"><span class="text-warning">CityArt</span> Artes Gráficas</b><br/>
 
-            <i class="fa fa-phone"></i> 14 3208-5654
-            <i class="ionicons ion-social-whatsapp"></i> 14 99777-4155<br/>
+            <span class="footer-phone"><i class="fa fa-phone"></i> 14 3208-5654</span>
+            <span><i class="ionicons ion-social-whatsapp"></i> 14 99777-4155</span><br/>
 
             <a href="mailto:contato@cityart.com.br">
                 <i class="fa fa-envelope"></i> contato@cityart.com.br
