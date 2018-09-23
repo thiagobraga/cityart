8868d1dc0244a47a9e8a85d0abcdd8df234e3a8a
Thiago Braga <thibraga06@gmail.com>
2018-09-23 11:14:13 -0300

    Atualizando BrowserSync no scripts.php
    Atualizei a versão do BrowserSync e também refatorei o código, evitando
    usar document.write() que é uma Violation, segundo o Chrome.

--- application/views/_template/scripts.php
+++ application/views/_template/scripts.php
@@ -6,8 +6,6 @@
     <?php }
 }
 
-if (ENVIRONMENT === 'development' && @get_headers("http://{$_SERVER['REMOTE_ADDR']}:3001")) { ?>
-    <script id="__bs_script__">
-        document.write('<script async src="http://127.0.0.1:3001/browser-sync/browser-sync-client.js?v=2.18.6"><\/script>'.replace('HOST', location.hostname));
-    </script>
+if (ENVIRONMENT === 'development' && @get_headers("http://{$_SERVER['REMOTE_ADDR']}:3000")) { ?>
+  <script async src="http://<?php echo $_SERVER['REMOTE_ADDR'] ?>:3000/browser-sync/browser-sync-client.js?v=2.24.7"></script>
 <?php } ?>
