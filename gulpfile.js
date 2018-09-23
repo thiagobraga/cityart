d6842c53219b340f7ed7c53b65e5ac3c9cfbe4b7
Thiago Braga <thibraga06@gmail.com>
2018-09-23 11:33:43 -0300

    Adequações no gulpfile para Gulp 4
    Além de modificar o funcionamento de algumas tasks e mover a task do
    BrowserSync, também fiz algumas correções de arquivos minificados.
    
    Também aproveitei esse commit para versionar uma quebra de linha no
    docker-compose.yml, bem como uma alteração no package-lock.json após
    executar o projeto novamente.

--- gulpfile.js
+++ gulpfile.js
@@ -1,100 +1,84 @@
 const gulp     = require('gulp'),
-  changed      = require('gulp-changed'),
-  sass         = require('gulp-sass'),
   autoprefixer = require('gulp-autoprefixer'),
-  uglify       = require('gulp-uglify'),
+  browserSync  = require('browser-sync'),
   concat       = require('gulp-concat'),
   rename       = require('gulp-rename'),
-  imagemin     = require('gulp-imagemin'),
-  browserSync  = require('browser-sync'),
-
-  images = {
-    src:  './assets/images/src/**/*',
-    dest: './assets/images/dist'
-  },
+  sass         = require('gulp-sass'),
+  uglify       = require('gulp-uglify'),
 
   styles = {
-    base:    './assets/scss',
-    src:     './assets/scss/**/*.scss',
-    dest:    './assets/css/dist',
-    options: { outputStyle: 'compressed' }
+    base: 'assets/scss',
+    src: 'assets/scss/**/*.scss',
+    dest: 'assets/css/dist'
   },
 
   scripts = {
     src: [
-      './node_modules/angular/angular.min.js',
-      './node_modules/ngmap/build/scripts/ng-map.min.js',
-      './node_modules/jquery/dist/jquery.min.js',
-      './node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
-      './node_modules/noty/js/noty/packaged/jquery.noty.packaged.min.js',
-      './assets/js/src/**/*.js'
+      'node_modules/angular/angular.min.js',
+      'node_modules/ngmap/build/scripts/ng-map.min.js',
+      'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+      'node_modules/noty/js/noty/packaged/jquery.noty.packaged.min.js',
+      'assets/js/src/**/*.js'
     ],
-    base: './assets/js/src',
-    dest: './assets/js/dist'
+    base: 'assets/js/src',
+    dest: 'assets/js/dist'
   },
 
   bsFiles = [
-    '!./assets/css/dist/**/*.min.css',
-    '!./assets/js/dist/**/*.min.js',
-    '!./application/logs/**/*.php',
-    './assets/css/dist/**/*.css',
-    './assets/js/dist/**/*.js',
-    './assets/images/dist/**/*.{png,jpg,jpeg,gif,svg,ico}',
-    './**/*.php'
+    '!assets/css/dist/**/*.min.css',
+    '!assets/js/dist/**/*.min.js',
+    'assets/css/dist/**/*.css',
+    'assets/js/dist/**/*.js',
+    '**/*.php'
   ];
-
-// Images
-gulp.task('images', function () {
-  return gulp.src(images.src)
-    .pipe(changed(images.dest))
-    .pipe(imagemin())
-    .pipe(gulp.dest(images.dest))
-    .pipe(browserSync.reload({ stream: true }));
-});
 
 // Styles
 gulp.task('styles', function () {
   return gulp.src(styles.src, { base: styles.base })
-    .pipe(sass(styles.options).on('error', sass.logError))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(autoprefixer('last 2 version'))
     .pipe(gulp.dest(styles.dest))
     .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('styles:prod', function () {
+  return gulp.src(styles.src, { base: styles.base })
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(styles.dest));
 });
 
 // Scripts
 gulp.task('scripts', function () {
   return gulp.src(scripts.src)
     .pipe(concat('scripts.js'))
-    .pipe(gulp.dest(scripts.dest))
-    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(scripts.dest));
+});
+
+gulp.task('scripts:prod', function () {
+  return gulp.src(scripts.src)
+    .pipe(concat('scripts.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest(scripts.dest));
 });
 
-// BrowserSync
-gulp.task('browser-sync', function () {
-  browserSync.init({
-    logPrefix:  'CityArt',
-    host:       'local.cityart.com.br',
-    files:      bsFiles,
-    port:       3001,
-    open:       false,
-    notify:     false,
-    ui:         false,
-    logSnippet: false
-  });
-});
 
 // Default
-gulp.task('default', ['images', 'styles', 'scripts'], function () {
-  gulp.start('browser-sync');
+gulp.task('default', gulp.series('styles', 'scripts', function () {
+  browserSync.init({
+    files: bsFiles,
+    logSnippet: false,
+    ghostMode: false,
+    notify: false,
+    open: false,
+    ui: false,
+  });
 
-  gulp.watch(images.src, ['images']);
-  gulp.watch(styles.src, ['styles']);
-  gulp.watch(scripts.src, ['scripts']);
-});
+  gulp.watch(styles.src, gulp.series('styles'));
+  gulp.watch(scripts.src, gulp.series('scripts'));
+}));
 
 // Production
-gulp.task('production', function () {
-  gulp.start('images', 'styles', 'scripts');
-});
+gulp.task('production', gulp.series('styles:prod', 'scripts:prod'));
